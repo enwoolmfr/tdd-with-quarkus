a03c4b9a9cf8073896ddd4a29ec9edad00a424f7
enwoolmfr <laurent.ducamp@ext.leroymerlin.fr>
2025-05-20 19:58:41 +0200

    fix(FizzBuzz): fix the case 3

--- src/main/java/org/acme/FizzBuzz.java
+++ src/main/java/org/acme/FizzBuzz.java
@@ -3,6 +3,9 @@ package org.acme;
 public class FizzBuzz {
 
     public String compute(int value){
+        if(value%3==0){
+            return "Fizz";
+        }
         return String.valueOf(value);
     } 
 }
