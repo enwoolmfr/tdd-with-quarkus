4c84b822b926ccfb6ba6f060207079c2530d5942
enwoolmfr <laurent.ducamp@ext.leroymerlin.fr>
2025-05-20 20:33:48 +0200

    feat(FizzBuzz): implement and refactor the code

--- src/main/java/org/acme/FizzBuzz.java
+++ src/main/java/org/acme/FizzBuzz.java
@@ -3,13 +3,14 @@ package org.acme;
 public class FizzBuzz {
 
     public String compute(int value){
-        if(value%5==0){
+        if(value%3==0 && value%5==0){
+            return "Fizz Buzz";
+        }else if(value%5==0){
             return "Buzz";
-        }
-
-        if(value%3==0){
+        }else if(value%3==0){
             return "Fizz";
+        }else{
+            return String.valueOf(value);
         }
-        return String.valueOf(value);
     } 
 }
